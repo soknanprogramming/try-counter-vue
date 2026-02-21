bad68b4cbc0aaf6b0a09671f6d316ff1b0cd29ff
soknanprogramming <poch.soknan@gmail.com>
2026-02-21 10:58:52 +0700

    basic tic tac toe

---
+++ src/components/EachGrid.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps<{
+  value: string
+}>()
+</script>
+
+<template>
+    <button :class="'bg-amber-200 h-20 m-1.5'">
+        {{ value }}
+    </button>
+</template>
